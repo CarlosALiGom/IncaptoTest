338e0b10287532450b6199880f91501e23d94e72
Carlos Aliaga <carlosaligom@gmai.com>
2023-09-21 18:47:41 +0200

    Create validateMiddleware, CustomRequest, and implement it on app

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import morgan from "morgan";
+import validate from "./middlewares/validateMiddleware/validateMiddleware.js";
 
 const app = express();
 
 app.use(morgan("dev"));
 
 app.use(express.json());
+
+app.post("/robot", validate);
 
 export default app;
